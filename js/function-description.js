1725122e6fd9ab3fe517ad356cf84e8d364b7cca
Victor HUGO <affonsovictor.dev@outlook.com>
2023-02-24 14:46:12 -0300

    project att 02

--- js/function-description.js
+++ js/function-description.js
@@ -2,6 +2,7 @@
 var image01=document.getElementById("txt-img-01"); 
 var image02=document.getElementById("txt-img-02");
 var image03=document.getElementById("txt-img-03");
+var image04=document.getElementById("txt-img-04");
 
 
 function hoverImage(){ 
@@ -34,5 +35,14 @@ function showImage03(){
     image03.style.display="inline";
 };
 
+function hoverImage04(){ 
+
+    image04.style.display="none";               
+};
+
+function showImage04(){
+
+    image04.style.display="inline";
+};
 
 
